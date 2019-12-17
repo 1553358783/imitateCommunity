8b1cb4e896c086f6d5b21c17daf6cde871bc1e13
MGZ <1553358783@qq.com>
2019-12-17 17:27:25 +0800

    init

---
+++ src/main/java/com/example/imitatecommunity/ImitatecommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.imitatecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImitatecommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImitatecommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imitatecommunity/ImitatecommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imitatecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImitatecommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
